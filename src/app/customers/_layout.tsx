e4598b35637b6f6a2a17a37cdbc71ba93e32c7c1
quirinof <matheusquinoff@gmail.com>
2025-06-08 11:20:05 -0300

    feat: customer page back button

--- src/app/customers/_layout.tsx
+++ src/app/customers/_layout.tsx
@@ -1,12 +1,22 @@
 import { Stack } from "expo-router";
+import { Link } from "expo-router";
+import { Ionicons } from "@expo/vector-icons";
 
 export default function CustomersLayout() {
   return (
-    <Stack
-      screenOptions={{
-        headerTintColor: "#2e86de",
-        headerTitle: "Detalhes do Cliente",
-      }}
-    />
+    <Stack>
+      <Stack.Screen
+        name="[id]"
+        options={{
+          headerTitle: "",
+          headerTintColor: "#2e86de",
+          headerLeft: () => (
+            <Link href="/drawer/customers" asChild>
+              <Ionicons name="arrow-back" size={24} color="#2e86de" />
+            </Link>
+          ),
+        }}
+      />
+    </Stack>
   );
 }
