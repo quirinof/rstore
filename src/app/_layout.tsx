0bff358058a131cfffa8ba676af52d9f11c55005
quirinof <matheusquinoff@gmail.com>
2025-06-17 15:40:37 -0300

    refactor: change customers tab

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,5 +1,11 @@
 import { Stack } from "expo-router";
+import { useEffect } from "react";
+import { createTables } from "@/lib/schema/createTables";
 
 export default function RootLayout() {
+  useEffect(() => {
+    createTables();
+  }, []);
+
   return <Stack screenOptions={{ headerShown: false }} />;
 }
